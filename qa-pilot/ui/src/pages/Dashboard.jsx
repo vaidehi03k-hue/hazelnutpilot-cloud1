b8dbeeb35f44f7f278b6ebe7b9984e1fde680452
Vaidehi Kulkarni <vaidehi03k@gmail.com>
2025-08-25 17:37:32 +0530

    Update Dashboard.jsx\

--- qa-pilot/ui/src/pages/Dashboard.jsx
+++ qa-pilot/ui/src/pages/Dashboard.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
-import api from '../api'
+import { api } from '../api'
 import { PieChart, Pie, Cell, BarChart, Bar, XAxis, YAxis, Tooltip, ResponsiveContainer } from 'recharts'
 import UICard from '../components/UICard'
 
